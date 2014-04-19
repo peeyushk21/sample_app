df397054a641a7e0f4938ba9e7d53726575e9d46
Peeyush <pshkmr96@gmail.com>
2014-04-19 19:20:23 +0530

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+   it "should have the title 'Home'" do
+      visit'/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
 
   describe "Help page" do
@@ -13,6 +17,10 @@ describe "Static pages" do
 		visit '/static_pages/help'
 		expect(page).to have_content('Help')
 	end
+  it "should have the title 'Help'" do
+      visit'/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About page" do
@@ -20,6 +28,10 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About us')
   	end
+    it "should have the title 'About us'" do
+      visit'/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About us')
+    end
   end
 end
 

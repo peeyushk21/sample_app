66300e6f2b8a4864e85c92578cc4cc5c8ca9ba83
Peeyush <pshkmr96@gmail.com>
2014-04-19 22:51:49 +0530

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+
 describe "Static pages" do
+
+let(:base_title) {'Ruby on Rails Tutorial Sample App'}
+
   describe "Home page" do
     it "should have the content 'sample app'" do
       visit '/static_pages/home'
@@ -8,7 +12,7 @@ describe "Static pages" do
     end
    it "should have the title 'Home'" do
       visit'/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -19,7 +23,7 @@ describe "Static pages" do
 	end
   it "should have the title 'Help'" do
       visit'/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -30,8 +34,19 @@ describe "Static pages" do
   	end
     it "should have the title 'About us'" do
       visit'/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About us')
+      expect(page).to have_title("#{base_title} | About us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+     visit '/static_pages/contact'
+     expect(page).to have_content('Contact')
+   end
+    it "should have the title 'Contact'" do
+    visit '/static_pages/contact'
+    expect(page).to have_title("#{base_title} | Contact")
+    end
+ end
 end
 
